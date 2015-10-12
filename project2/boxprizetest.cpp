32e991daf32ec12ee69fbf109a826db551912833
codingkitty81 <codingkitty81@gmail.com>
2015-10-12 19:37:34 +0000

    boxprizetest.cpp

--- project2/boxprizetest.cpp
+++ project2/boxprizetest.cpp
@@ -11,6 +11,7 @@
 #include "box.h"
 #include "prize.h"
 #include "CinReader.h"
+#include "boxprizetest.h"
 
 #include <cstdlib>
 #include <iostream>
@@ -18,33 +19,6 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-
-/**
- * The main menu allowing the user to go to menus to run an interactive test
- * for the prize and box classes, or run an automated unit test.
- */
-void mainMenu ();
-
-/**
- * The menu giving options to test various components of the box class. The
- * user can add, view, and remove prizes. The box's color and number can be
- * changed. The user can also view the capacity of the box and the number of
- * prizes currently in the box.
- */
-void boxMenu ();
-
-/**
- * The menu giving options to test components of the prize class. The user can
- * change the name and value of the prize. The user is also able to compare
- * two prizes by entering in the names and values of the two prizes.
- */
-void prizeMenu ();
-
-/**
- * Pauses the program, requiring enter to be pressed to continue. This function
- * clears the screen before the next function is executed.
- */
-void pause ();
 
 /* for unit testing -- do not alter */
 template <typename X, typename A>
