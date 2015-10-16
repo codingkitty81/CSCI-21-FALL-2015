11f77395824787f88cabb2a139040eb04cbfc4e2
codingkitty81 <codingkitty81@gmail.com>
2015-10-16 16:53:04 +0000

    SLNode.cpp

---
+++ pc18/SLNode.cpp
@@ -0,0 +1,28 @@
+#include "SLNode.h"
+
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+SLNode::SLNode () : nextNode(NULL), contents(0) {}
+
+SLNode::SLNode (int newContents) : nextNode(NULL), contents(newContents) {}
+
+SLNode::~SLNode () {}
+
+void SLNode::setContents (int newContents) {
+	contents = newContents;
+}
+
+int SLNode::getContents () const {
+	return contents;
+}
+
+void SLNode::setNextNode (SLNode* newNextNode) {
+	nextNode = newNextNode;
+}
+
+SLNode* SLNode::getNextNode () const {
+	return nextNode;
+}
