dbf09df4e65e257de49d3d2e6dec6575b1d887f8
codingkitty81 <codingkitty81@gmail.com>
2015-11-04 20:21:19 +0000

    project3-test.h

---
+++ project3/project3-test.h
@@ -0,0 +1,13 @@
+/*
+ * project3-test.h file for Programming Project #3
+ * This file contains the function definitions for project3-test.cpp.
+ *
+ * Katherine Jouzapaitis
+ * Date created: 10/3/2015
+ * Date last modified: 10/4/2015
+ *
+ * Resources:
+ *http://stackoverflow.com/questions/7868936/read-file-line-by-line
+ */
+
+void fileProcess(string fileName);
